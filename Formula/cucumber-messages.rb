b775b198ec8434d9f8492cab601db8e0f00f65bc
jreleaserbot <jreleaser@kordamp.org>
2026-04-05 20:04:02 +0000

    cucumber-messages-standalone v0.2.13

--- Formula/cucumber-messages.rb
+++ Formula/cucumber-messages.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.23.0 at 2026-04-05T20:01:49.947646734Z
+# Generated with JReleaser 1.23.0 at 2026-04-05T20:03:49.019341124Z
 
 class CucumberMessages < Formula
   desc "A commandline interface to work with Cucumber Messages"
@@ -8,19 +8,19 @@ class CucumberMessages < Formula
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/cucumber/messages-cli/releases/download/v0.2.13/cucumber-messages-standalone-0.2.13-linux-aarch_64.zip"
-    sha256 "93d079ff1e52e6055a90a96b9abe2fea011b039867225f8e5cc81d6ff390e528"
+    sha256 "279fab5a6cca9ed1f896009861727fee3812d703fa72d2eb2c2e9e9e3025bac7"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/cucumber/messages-cli/releases/download/v0.2.13/cucumber-messages-standalone-0.2.13-linux-x86_64.zip"
-    sha256 "bedecf386612a03c5d73a34a9880b7c4044517a7dd33ee99c38e56918a7f03f7"
+    sha256 "9bd96a8271c032b5f7145c2c2ed3ea74ff9ea6f9a267b59c7ff1baeaa9d1715e"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/cucumber/messages-cli/releases/download/v0.2.13/cucumber-messages-standalone-0.2.13-osx-aarch_64.zip"
-    sha256 "080791fd8d25554710e34f7d8f96b3326e5b554d3779091fa07e83c1c775ef96"
+    sha256 "6951c58b9119240ce99281054b35c952473bce4adccfb42bb785b2680d56cf17"
   end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/cucumber/messages-cli/releases/download/v0.2.13/cucumber-messages-standalone-0.2.13-osx-x86_64.zip"
-    sha256 "abb4ecd265380e1afd6b3a5eb8a4920e7bc242f6a57c9709c7155fdada215004"
+    sha256 "319fb24338315fc8dd76a220ec2854ec67b59afe86d7b1e0871fa7f44411a8d6"
   end
 
 
